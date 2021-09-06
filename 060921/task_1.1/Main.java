2150c2d3f8e3ef369f7722f74eb671127c129632
kardan1984 <90193923+kardan1984@users.noreply.github.com>
2021-09-06 15:45:36 +0300

    Update Main.java

--- 060921/task_1.1/Main.java
+++ 060921/task_1.1/Main.java
@@ -1,1 +1,25 @@
+/* 
+Напиши программу, которая в методе main объявляет такие переменные:
+name типа String, age типа int и city типа String.
+Примечание: "объявить переменную" - значит то же, что и "создать переменную".
+Требования:
+•
+Объяви переменную name типа String.
+•
+Объяви переменную age типа int.
+•
+Объяви переменную city типа String.
+•
+Должны быть объявлены 3 переменные.
+*/
 
+public class Main {
+    public static void main(String[] args) {
+        String name = "Alex";
+        System.out.println("My name is "+name);
+        int age = 35;
+        System.out.println("I am "+age+" years old");
+        String city = "Moscow";
+        System.out.println("I am from "+city);
+    }
+}
