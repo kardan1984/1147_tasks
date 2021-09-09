b33c1490ba63b4a03a6cc908b15ba4e245f61a87
kardan1984 <90193923+kardan1984@users.noreply.github.com>
2021-09-09 20:55:40 +0300

    Update Main.java

--- 080921/task_2.8/Main.java
+++ 080921/task_2.8/Main.java
@@ -1,1 +1,14 @@
+/*
+Задание: Дан массив с элементами [2, 3, 4, 5]. С помощью цикла for найдите произведение элементов этого массива.
+*/
 
+public class Main {
+    public static void main(String[] args) {
+        int [] arr = {2,3,4,5};
+        int mult = 1;
+        for (int i = 0; i <4 ; i++) {
+            mult*=arr[i];
+        }
+        System.out.println(mult);
+    }
+}
