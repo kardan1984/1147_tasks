31d1a14e2c498879f5afd51d5f5d2776b9737628
kardan1984 <90193923+kardan1984@users.noreply.github.com>
2021-09-10 16:51:16 +0300

    Update Main.java

--- 100921/task_3.3/Main.java
+++ 100921/task_3.3/Main.java
@@ -1,3 +1,7 @@
+/*
+Задание: Сделайте функцию, которая параметрами принимает 2 числа. Если эти числа равны - пусть функция вернет true, а если не равны - false.
+*/
+
 public class Main {
     public static void main(String[] args) {
     System.out.println(digits (5,6));
