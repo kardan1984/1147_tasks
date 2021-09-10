6195d6d02216db06871f115fb2e54dc038d76f7e
kardan1984 <90193923+kardan1984@users.noreply.github.com>
2021-09-10 15:26:23 +0300

    Update Main.java

--- 080921/task_2.10/Main.java
+++ 080921/task_2.10/Main.java
@@ -5,12 +5,15 @@
 public class Main {
     public static void main(String[] args) {
         //напишите тут ваш код
-        int [] arr = {2,3,4,5,5,6,7};
-        for (int i = 0; i < 7; i++) {
+        int [] arr = {2,3,4,5,3,7,7};
+        int count = 0;
+        for (int i = 0; i < arr.length-1; i++) {
             if (arr[i]==arr[i+1])
-            System.out.println("ДА");
-            else
-                System.out.println("НЕТ");
+                count++;
         }
+        if (count>0)
+        System.out.println("ДА");
+        else
+        System.out.println("НЕТ");
     }
 }
