74ba1e25530002d9e0315fb722d0cb0bfdc27682
kardan1984 <90193923+kardan1984@users.noreply.github.com>
2021-09-10 16:49:51 +0300

    Update Main.java

--- 100921/task_3.5/Main.java
+++ 100921/task_3.5/Main.java
@@ -1,1 +1,15 @@
+/*
+Задание: Сделайте функцию, которая параметрами принимает 2 числа. Если их сумма больше 10 - пусть функция вернет true, а если нет - false.
+*/
 
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(digits (5,6));
+    }
+    public static boolean digits (int a, int b){
+        if (a+b>10)
+            return true;
+        else
+            return false;
+
+    }
