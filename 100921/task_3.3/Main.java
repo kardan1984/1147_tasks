693e428221cb94d90a9f46769770d35ee36711ba
kardan1984 <90193923+kardan1984@users.noreply.github.com>
2021-09-10 16:48:30 +0300

    Update Main.java

--- 100921/task_3.3/Main.java
+++ 100921/task_3.3/Main.java
@@ -1,1 +1,11 @@
-
+public class Main {
+    public static void main(String[] args) {
+    System.out.println(digits (5,6));
+    }
+    public static boolean digits (int a, int b){
+        if (a==b)
+            return true;
+        else
+            return false;
+    }
+}
