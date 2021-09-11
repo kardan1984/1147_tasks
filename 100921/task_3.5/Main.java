15db431cdd530d7c88f92732731f3944368fb01a
kardan1984 <90193923+kardan1984@users.noreply.github.com>
2021-09-11 17:50:50 +0300

    Update Main.java

--- 100921/task_3.5/Main.java
+++ 100921/task_3.5/Main.java
@@ -1,1 +1,15 @@
+/*
+Задание: Дан массив с числами. Выведите последовательно его элементы используя рекурсию и не используя цикл.
+*/
 
+public class Main {
+    public static void main(String[] args) {
+        int num[] = {4, 2, 6, 2, 65};
+        int i = 0;
+        F(i,num);
+    }
+    public static void F(int i, int num[]) {
+        if (i == (num.length - 1)) System.out.println(num[i]);
+        else {System.out.println(num[i]);F(i+1,num);}
+    }
+}
