9a8eef6841db15010404a06dd8b772fd96cff225
kardan1984 <90193923+kardan1984@users.noreply.github.com>
2021-09-18 16:49:22 +0300

    Update Main.java

--- 130921/task_4.8/Main.java
+++ 130921/task_4.8/Main.java
@@ -1,1 +1,81 @@
+import java.util.Scanner;
 
+/*
+ *  ***Гостиница***
+ *  1) Гостиница
+ *  2) Номера (кол-во мест(1-3), сан.узел(есть/нет), питаение(есть/нет), wifi(есть/нет), свободен/занят)
+ *
+ *   *Задание для лабораторной работы №4*
+ *   Освободить комнату
+ *   Вывести свойства комнаты
+ *   Показать комнаты с WiFi
+ *   Показать комнаты с WC
+ *   Показать комнаты с Eat
+ *   Показать комнаты по кол-ву спальных мест
+ *   Отобразить список команд
+ * */
+public class Main {
+    public static void main(String[] args) {
+        Room[] rooms = {
+                new Room((byte) 1,false,false,true,(byte) 11),
+                new Room((byte) 2,true,true,false,(byte) 12),
+                new Room((byte) 1,false,true,true,(byte) 13),
+                new Room((byte) 3,true,false,false,(byte) 21),
+                new Room((byte) 2,false,false,false,(byte) 22),
+                new Room((byte) 1,true,true,true,(byte) 23),
+                new Room((byte) 3,false,true,false,(byte) 31),
+                new Room((byte) 3,true,true,false,(byte) 32),
+                new Room((byte) 1,false,false,true,(byte) 33),
+        };
+
+        String help = "*----*\n" +
+                "getFreeRooms - показать все свободные комнаты\n" +
+                "getWifiRooms - показать все свободные комнаты с Wifi\n" +
+                "getWСRooms - показать все свободные комнаты c WC\n" +
+                "getEatRooms - показать все свободные комнаты c питанием\n" +
+                "getQTRooms - показать все свободные комнаты по нужному кол-ву мест\n" +
+                "reserveRoom - забронировать комнату\n" +
+                "setRoomFree - освободить комнату\n" +
+                "roomInfo - посмотреть параметры номера\n" +
+                "exit - выход из программы\n" +
+                "help - вывод подсказки по командам\n" +
+                "*----*";
+        System.out.println(help);
+        Hotel hotel = new Hotel(rooms);
+        Scanner scanner = new Scanner(System.in);
+        String command;
+        while (true){
+            System.out.println("Введите команду");
+            command = scanner.nextLine();
+            if(command.equals("getFreeRooms")) {
+                hotel.getFreeRooms();
+            }else if(command.equals("getWifiRooms")) {
+                hotel.getWifiRooms();
+            }else if(command.equals("getWСRooms")) {
+                hotel.getWCRooms();
+            }else if(command.equals("getEatRooms")) {
+                hotel.getEatRooms();
+            }else if(command.equals("getQTRooms")) {
+                System.out.print("Введите необходимое кол-во спальных мест: ");
+                byte quantity = (byte) scanner.nextInt();
+                hotel.getQTRooms(quantity);
+            }else if(command.equals("reserveRoom")){
+                System.out.print("Введите номер комнаты для бронирования: ");
+                byte roomNumber = (byte) scanner.nextInt();
+                hotel.reserveRoom(roomNumber);
+            }else if(command.equals("setRoomFree")){
+                System.out.print("Введите номер комнаты для освобождения: ");
+                byte roomNumber = (byte) scanner.nextInt();
+                hotel.setRoomFree(roomNumber);
+            }else if(command.equals("roomInfo")){
+                System.out.print("Введите номер комнаты для получения информации: ");
+                byte roomNumber = (byte) scanner.nextInt();
+                hotel.roomInfo(roomNumber);
+            }else if(command.equals("help")){
+                System.out.println(help);
+            }else if (command.equals("exit")){
+                break;
+            }
+        }
+    }
+}
