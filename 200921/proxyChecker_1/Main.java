f99b35a697cf3187a61a3703abbb08f2b293c884
kardan1984 <90193923+kardan1984@users.noreply.github.com>
2021-09-20 17:27:22 +0300

    Create Main.java

---
+++ 200921/proxyChecker_1/Main.java
@@ -0,0 +1,136 @@
+/*import java.io.*;
+import java.net.InetSocketAddress;
+import java.net.Proxy;
+import java.net.URL;
+import java.net.URLConnection;*/
+
+/*public class Main {
+    public static void main(String[] args) {*/
+/*        String text = "Hello world";
+        try{
+            FileOutputStream fos = new FileOutputStream("C:/java/test.txt");
+            byte[] buffer = text.getBytes();
+            fos.write(buffer);
+        }catch (
+                IOException ex){
+            ex.printStackTrace();
+        }
+        System.out.println("The file has been written");
+    }
+}*/
+/*
+        try {
+            FileInputStream fin = new FileInputStream("C:\\java\\test.txt");
+            int i;
+            while ((i=fin.read()) != -1){
+                System.out.print((char) i);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}*/
+
+/*
+        try {
+            Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress(200.136.74.121,8080));
+            URL url = new URL("https://vozhzhaev.ru/test.php");
+            URLConnection URLConnection = url.openConnection(proxy);
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(URLConnection.getInputStream())
+            );
+            String inputLine;
+            while ((inputLine = in.readLine()) != null)
+                System.out.println(inputLine);
+            in.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}*/
+import java.io.*;
+import java.net.InetSocketAddress;
+import java.net.Proxy;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            FileInputStream fin = new FileInputStream("C:/java/ip.txt");
+            int i;
+            String result = "";
+            while ((i = fin.read()) != -1) {
+                if (i == 13) continue; // Возврат каретки, то пропускаем иттерацию
+                else if (i == 10) { // Перенос строки
+                    String ip = result.split(":")[0];
+                    int port = Integer.parseInt(result.split(":")[1]);
+                    result = "";
+                    checkProxyClass checkProxy = new checkProxyClass(ip,port);
+                    checkProxy.start();
+                } else if (i == 9) { // Табуляция
+                    result += ":";
+                } else { // Остальные символы
+                    result += (char) i;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
+
+    // Метод checkProxy принимает на вход ip и port которые мы читаем построчно из файла
+    // Результат выводит на экран
+    // Если ip рабочий, то сервер нам его возваращает
+    // Если нет, то случается Exception и мы пишем на экран, что ip не работает!
+
+
+ class checkProxyClass extends Thread {
+    String ip;
+    int port;
+
+    public checkProxyClass(String ip, int port) {
+        this.ip = ip;
+        this.port = port;
+    }
+     @Override
+     public void run() {
+        checkProxy(ip,port);
+     }
+
+    private void checkProxy(String ip, int port) {
+        try {
+            // Создаём объект прокси
+            Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress(ip, port));
+            // Указываем к какому ресурсу нужно подключиться, данный ресурс https://vozhzhaev.ru/test.php
+            // в ответ всегда отдаст вам ip
+            URL url = new URL("https://vozhzhaev.ru/test.php");
+            // Открываем соединение
+            URLConnection URLConnection = url.openConnection(proxy);
+            // Сохраняем ответ в поток ввода
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(URLConnection.getInputStream())
+            );
+            // Переменная в которую в формате сроки кладём ответ от сервера
+            String inputLine;
+            // Читаем ответ от сервера пока не встретится null
+            while ((inputLine = in.readLine()) != null) {
+                System.out.println("IP: " + inputLine + " - работает!");
+                String str = (ip + ":" + port + "\n");
+                FileOutputStream fos = new FileOutputStream("C:/java/good_ip.txt", true);
+                byte[] buffer = str.getBytes();
+                fos.write(buffer);
+            }
+
+            // Закрываем поток ввода
+            in.close();
+
+        } catch (IOException e) {
+            // Если ip не доступен, то бужет Exception, но мы его обрабатываем и выводим инфо на экран
+            System.out.println("IP: " + ip + " не работает!");
+        }
+    }
+}
